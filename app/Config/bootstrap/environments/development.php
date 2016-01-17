ed0acb449021a23c71acbfc11032f597581ff87f
Hirokazu Kano <kano@develop.com>
2016-01-17 05:35:29 +0000

    BDD プラグインの追加

--- app/Config/bootstrap/environments/development.php
+++ app/Config/bootstrap/environments/development.php
@@ -8,4 +8,7 @@ Environment::configure('development', true, [
 	'MYSQL_TEST_DB_NAME' => 'test_blog',
 	'MYSQL_PREFIX' => '',
 
-]);
+    ], function() {
+        CakePlugin::load('Bdd');
+        //CakePlugin::load('Fabricate');
+});
