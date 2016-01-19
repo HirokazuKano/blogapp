945d5a409ef6dc76be24b0c476be7d26e3f1a18d
Hirokazu Kano <kano@develop.com>
2016-01-19 07:49:53 +0000

    テストデータはFabricateを使って生成するように変更

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -12,19 +12,6 @@ class PostFixture extends CakeTestFixture {
  */
 	public $import = array('model' => 'Post');
 
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id' => 1,
-			'title' => 'Lorem ipsum dolor sit amet',
-			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-			'created' => '2016-01-19 07:06:26',
-			'modified' => '2016-01-19 07:06:26'
-		),
-	);
+
 
 }
