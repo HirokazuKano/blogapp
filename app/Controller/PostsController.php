359809160445166b9f1f19354155599fd0a592b3
Hirokazu Kano <kano@develop.com>
2016-01-20 02:57:08 +0000

    ステップ実装しフィーチャーテストが通過するようになる

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -23,6 +23,11 @@ class PostsController extends AppController {
     ];
 
     public function index() {
+        $this->Paginator->settings = [
+            'limit' => 5,
+            'order' => ['Post.id' => 'desc'],
+        ];
+
         $this->set('posts', $this->Paginator->paginate());
     }
     public function add() {
